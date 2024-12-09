3b8a5ba1b682ac5e25911edbdc53dee88308dcc8
Danielius Jurna <danielius.jurna@metasite.net>
2024-12-09 09:37:37 +0200

    Customer projection

--- src/main/java/com/example/customer/CustomerView.java
+++ src/main/java/com/example/customer/CustomerView.java
@@ -1,7 +1,12 @@
 package com.example.customer;
 
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.bson.codecs.pojo.annotations.BsonRepresentation;
+
 import io.micronaut.core.annotation.Introspected;
 
+import static org.bson.BsonType.OBJECT_ID;
+
 @Introspected
-public record CustomerView(String id, String firstName, String lastName) {
+public record CustomerView(@BsonId @BsonRepresentation(OBJECT_ID) String id, String firstName, String lastName) {
 }
